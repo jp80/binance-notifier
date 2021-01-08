45ce76ac20957fd9424ff856678584af4b79a3e2
jp <jamie@txt3.com>
2021-01-08 12:32:55 +0000

    removed /vendor and composer.lock to make .gitignore work

--- vendor/larislackers/php-binance/docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Larislackers\\BinanceApi\\BinanceApiContainer"],["c","Larislackers\\BinanceApi\\Enums\\BasicEnum"],["c","Larislackers\\BinanceApi\\Enums\\BinanceApi"],["c","Larislackers\\BinanceApi\\Enums\\ConnectionDetails"],["c","Larislackers\\BinanceApi\\Exception\\BinanceApiException"],["c","Larislackers\\BinanceApi\\Exception\\LarislackersException"]];
